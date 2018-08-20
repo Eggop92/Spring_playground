a5cf294610c25ad0678f8749bd763d3f20038f55
Egoitz Puerta <egoitzpuerta@gmail.com>
2018-08-20 21:21:48 +0200

    Creado una interfaz con la base de datos con JPA

---
+++ src/main/java/seguros/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package seguros.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import seguros.models.User;
+
+//This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+//CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
