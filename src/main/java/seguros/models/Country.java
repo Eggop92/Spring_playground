56e23f8deec854bdf713f44ef39b31a93264f833
Egoitz Puerta <egoitzpuerta1992@gmail.com>
2018-08-24 20:18:36 +0200

    Añadido relacion entre usuario y pais

--- src/main/java/seguros/models/Country.java
+++ src/main/java/seguros/models/Country.java
@@ -1,9 +1,13 @@
 package seguros.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -15,6 +19,8 @@ public class Country{
     private Integer id;
     private String name;
 	private String phonePrefix;
+	@OneToMany(mappedBy = "country")
+    private List<User> users = new ArrayList<User>();
 	
 	public Integer getId(){return id;}
 	public void setId(Integer id){this.id=id;}
@@ -23,6 +29,8 @@ public class Country{
 	public void setName(String name){this.name = name;}
 
 	public String getPhonePrefix(){return phonePrefix;}
-	public void setPhonePrefix(String phonePrefix){this.phonePrefix = phonePrefix;
-	}
+	public void setPhonePrefix(String phonePrefix){this.phonePrefix = phonePrefix;}
+
+	public List<User> getUsers(){return users;}
+	public void setUsers(List<User> users){this.users=users;}
 }
--- src/main/java/seguros/models/User.java
+++ src/main/java/seguros/models/User.java
@@ -3,6 +3,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity // This tells Hibernate to make a table out of this class
@@ -11,34 +12,21 @@ public class User {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
-
     private String name;
+	private String email;
+	@ManyToOne
+	private Country country;
 
-    private String email;
+	public Integer getId() {return id;}
+	public void setId(Integer id) {	this.id = id;}
 
-	public Integer getId() {
-		return id;
-	}
+	public String getName() {return name;}
+	public void setName(String name) {this.name = name;}
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
+	public String getEmail() {return email;}
+	public void setEmail(String email) {this.email = email;	}
+	
+	public Country getCountry(){return country;}
+	public void setCountry(Country country){this.country = country;}
 
 }
